755ce55c02a8c1d3474503371590a26ed2345863
jaydotts <howard@ucsb.edu>
2023-06-07 10:26:38 -0700

    added config file rotation

--- check_network.sh
+++ check_network.sh
@@ -53,6 +53,8 @@ update_schedule () {
   local curr_file=$(jq -r '.status | keys_unsorted[]' "$SCHEDULE_PATH")
   local curr_cnt=$(jq -r '.status."'$curr_file'"' "$SCHEDULE_PATH")
   local next_file=$curr_file
+  updated_json=$(jq '' $SCHEDULE_PATH)
+
   local objects=$(jq -r '
                 .frequency | 
                 keys[] as $k | 
@@ -64,40 +66,78 @@ update_schedule () {
 
   echo "File: $curr_file"
   echo "Count: $curr_cnt"
-  echo $index
+  echo "Current index: $index"
 
-  if [ $curr_cnt -gt 0 ]; then 
-    echo "checking if key exists"
-    if [ -n $index ]; then
-      echo "key exists"
-      local max_cnt=$(jq --arg key $curr_file '.frequency[] |
-                    select(.template == $key) | 
-                    .repeats' $SCHEDULE_PATH)
-      echo $max_cnt
-      if [ $max_cnt -eq $curr_cnt ]; then 
-        # repeat config, increment counter, no change
-        ((curr_cnt++))
-        updated_json=$(jq --arg key $curr_file \
-                        --argjson value $curr_cnt '.status.key=$value' \
-                        $SCHEDULE_PATH)
-        echo "updated"
-      else 
-        # go to the next one 
-        local next=$order+1
-        local length=$(jq '.frequency | length' $SCHEDULE_PATH)
-        if [ $next > $length ]; then 
+  echo "checking if key exists"
+  if [[ -n $index ]]; then
+    echo "key exists"
+    local max_cnt=$(jq --arg key $curr_file '.frequency[] |
+                  select(.template == $key) | 
+                  .repeats' $SCHEDULE_PATH)
+    if [ $max_cnt -gt $curr_cnt ]; then 
+      echo "advancing counter..."
+      # repeat config, increment counter, no change
+      ((curr_cnt++))
+      updated_json=$(jq --arg key $curr_file \
+                      --argjson value $curr_cnt '.status[$key]=$value' \
+                      $SCHEDULE_PATH)
+    else 
+      echo "Moving to next config file"
+      # go to the next one in order
+      ((index++))
+      local length=$(jq '.frequency | length' $SCHEDULE_PATH)
+      echo "next index $index"
+      echo "length $length"
+      if [ $index -eq $length ]; then 
+      echo "end of list" 
         # end of list, return to top
         local init=$(jq '.frequency[] |
                       select(.order == 0) | .template' $SCHEDULE_PATH)
+        echo "Returning to $init"
         updated_json=$(jq --argjson key $init \
-                        '.status={ ($key):0 }' \
+                        '.status={ ($key):1 }' \
                         $SCHEDULE_PATH)
-        echo $updated_json
+      else 
         # move to next in order
-        fi 
-      fi
+        local next_file=$(jq --argjson order $index \
+                        '.frequency[] |
+                        select(.order == $order) | 
+                        .template' $SCHEDULE_PATH)
+        echo "New file: $next_file"
+        updated_json=$(jq --argjson key $next_file \
+                      '.status={ ($key):1 }' \
+                      $SCHEDULE_PATH)
+      fi 
     fi
+  else 
+    echo "key $curr_file does not exist. Resetting."
+    local init=$(jq '.frequency[] |
+              select(.order == 0) | .template' $SCHEDULE_PATH)
+    updated_json=$(jq --argjson key $init \
+          '.status={ ($key):1 }' \
+          $SCHEDULE_PATH)
+    echo $updated_json > tmp && mv tmp $SCHEDULE_PATH
   fi
+}
+
+update_configs () { 
+  update_schedule
+  local new_configs=$(echo $updated_json | jq '.status | keys_unsorted[0]' | tr -d '"')
+  local target=$HOME/dirpi/config_templates/$new_configs
+  if [ -f "$HOME/dirpi/config_templates/$new_configs" ]; then
+    echo "file found. copying to config.ini.." 
+    cp $target "$HOME/dirpi/config/config.ini"
+    echo "done. overwriting json" 
+    echo $updated_json > tmp && mv tmp $SCHEDULE_PATH
+  else 
+    echo "Config file not in templates."
+    echo $updated_json > tmp && mv tmp $SCHEDULE_PATH
+  fi
+}
+
+
+upsert_configs () {
+  echo "Upserting.."
 }
 
 # main execution 
@@ -105,7 +145,8 @@ if [ connected ]; then
   echo "Connection to tau.physics.ucsb.edu active"
   #copy_data
   #fetch_configs
-  update_schedule
+  update_configs
+  upsert_configs
 else
   echo "Connection to tau.physics.ucsb.edu is down"
 fi
